d01d7e20bd271a6b27a38fbb55cb7f96b6907f9d
ilabstaff <ilabstaff@172.16.116.67>
2019-10-23 17:49:57 -0700

    finish fortune cookie.

--- src/_06_duelling_buttons/DuelingButtons.java
+++ src/_06_duelling_buttons/DuelingButtons.java
@@ -6,6 +6,7 @@ package _06_duelling_buttons;
  */
 
 import java.awt.Dimension;
+import java.awt.Frame;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -27,33 +28,79 @@ public class DuelingButtons implements ActionListener {
 	public void run() {
 
 		// 1. Add the panel to the frame
+		
+		frame.add(panel);
+		
 
 		// 2. Make the frame visible
+		
+		frame.setVisible(true);
+		
 
 		// 3. Set the text of the leftButton to "Click me!"
+		
+		leftButton.setText("click me");
+		
+		
 
 		// 4. Set the text of the rightButton to "Click me!"
+		
+		rightButton.setText("click me");
+		
+		
+		
 
 		// 5. Add an action listener to the leftButton
+		
+		leftButton.addActionListener(this);
 
 		// 6. Add an action listener to the rightButton
+		
+		rightButton.addActionListener(this);
+		
 
 		// 7. Add the leftButton to the panel
+		
+		panel.add(rightButton);
 
 		// 8. Add the rightButton to the panel
+		
+		panel.add(leftButton);
 
 		// 9. Pack the frame
+		
+		frame.pack();
+		
 
 		// 10. Set the title of the frame to "Demanding Buttons"
+		
+		frame.setTitle("Demanding Buttons");
 
 	}
 
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		JButton buttonPressed = (JButton) arg0.getSource();
+	public void actionPerformed(ActionEvent E) {
+		JButton buttonPressed = (JButton) E.getSource();
 
 		/* If the buttonPressed was the leftButton.... */
+		if(buttonPressed == leftButton ) {
+			rightButton.setText("no, click me");
+			rightButton.setPreferredSize(BIG);
+			leftButton.setText("click me");;
+			leftButton.setPreferredSize(SMALL);
+			
+		}
+		
+		else if(buttonPressed == rightButton ) {
+			rightButton.setText("click me");
+			rightButton.setPreferredSize(SMALL);
+			leftButton.setText("no, click me");;
+			leftButton.setPreferredSize(BIG);
+			
+		}
+		
 		// Set the text of the rightButton to "No, click Me!"
+			
 		// Set the PREFERRED size of the rightButton to BIG
 		// Set the text of the leftButton to "Click Me!"
 		// Set the PREFERRED size of the leftButton to SMALL
---
+++ src/fortune_cookie2.java
@@ -0,0 +1,4 @@
+
+public class fortune_cookie2 {
+
+}
